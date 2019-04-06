84d8c07e87d8ec3e1789d2ab74ef11b5a90e56e0
Ibrahim <isalaheldin217@gmail.com>
2019-04-06 18:14:53 +0200

    Merge origin/master
    Conflicts:
    	BookStorePR/src/main/webapp/WEB-INF/web.xml

--- EmployeeGitTask/src/main/java/com/employeegit/database/DataBaseProperties.java
+++ EmployeeGitTask/src/main/java/com/employeegit/database/DataBaseProperties.java
@@ -5,10 +5,38 @@
  */
 package com.employeegit.database;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+
 /**
  *
  * @author Ibrahim
  */
 public class DataBaseProperties {
-    
+    Properties rootProperties = new Properties();
+            FileOutputStream propertiesFile = null;
+            Map<Object,Object> propertiesMap = new TreeMap<>();
+            propertiesMap.put("MYSQL_DB_URL","jdbc:mysql://localhost:3306/msql");
+            propertiesMap.put("MYSQL_DB_USERNAME", "root");
+            propertiesMap.put("MYSQL_DB_PASSWORD", "admin");
+            
+        try {
+            propertiesFile = new FileOutputStream("fileProp.properties");
+            rootProperties.putAll(propertiesMap);
+            rootProperties.store(propertiesFile,null);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }finally{
+             try {
+                 if(rootProperties == null) rootProperties.clear();
+                 if(propertiesFile == null) propertiesFile.close();
+             } catch (IOException ex) {
+                 Logger.getLogger(DataBaseProperties.class.getName()).log(Level.SEVERE, null, ex);
+             }
 }
